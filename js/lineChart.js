28bf5bebe85fda21c4e1433a4c6e38e502b7a365
ygao <ygao@cs.utah.edu>
2017-11-08 01:58:49 -0700

    update line

--- js/lineChart.js
+++ js/lineChart.js
@@ -35,30 +35,62 @@ class LineChart{
 
         console.log(thistable.banks);
 
+        //aggregation
+        let aggrebanks = [];
+        let k = 0;
+        let j = 0;
+        for(let i = 0; i < thistable.banks.length; i++){
+            if(i === thistable.banks.length -1){
+                aggrebanks.push({});
+                aggrebanks[k].efyear = thistable.banks[i].efyear;
+                aggrebanks[k].amount = j+1;
+                k++;
+                j = 0;
+            }else{
+                if(thistable.banks[i].efyear === thistable.banks[i+1].efyear){
+                    j++;
+                }else{
+                    aggrebanks.push({});
+                    aggrebanks[k].efyear = thistable.banks[i].efyear;
+                    aggrebanks[k].amount = j+1;
+                    k++;
+                    j = 0;
+                }
+            }
+        }
+        for(let i = 0; i < aggrebanks.length-1; i++){
+            aggrebanks[i].nextamount = aggrebanks[i+1].amount;
+        }
+        aggrebanks[aggrebanks.length-1].nextamount = null;
+       // console.log(aggrebanks);
+
         //scale
         this.xScale = d3.scaleLinear()
-            .domain([(d3.min(thistable.banks,d=>d.efyear)-1),(d3.max(thistable.banks,d=>d.efyear)+1)])
+            .domain([(d3.min(aggrebanks,d=>d.efyear)-1),(d3.max(aggrebanks,d=>d.efyear)+1)])
             .range([0,thistable.svgwidth]);
         this.yScale = d3.scaleLinear()
-            .domain([])
-            .range([]);
+            .domain([0,d3.max(aggrebanks,d=>d.amount)])
+            .range([0,thistable.svgHeight]);
 
         //draw lines
-        this.svg.selectAll("line").data(thistable.banks).enter().append("line")
+        this.svg.selectAll("line").data(aggrebanks).enter().append("line")
             .attr("x1",function (d) {
-                return thistable.xScale(d.efyear)
+                return thistable.xScale(d.efyear);
             })
-            .attr("y1",0)
+            .attr("y1",function (d) {
+                if(d.nextamount !== null){
+                    return thistable.yScale(d.amount);
+                }
+            })
             .attr("x2",function (d) {
                 return thistable.xScale(d.efyear + 1);
             })
-            .attr("y2",50);
-         //   .attr("stroke-dasharray",2)
-         //   .classed("lineChart",true);
-    }
-
-    update() {
-
+            .attr("y2",function (d) {
+                if(d.nextamount !== null){
+                    return thistable.yScale(d.nextamount);
+                }
+            })
+            .classed("lineChart",true);
     }
 
 }
