84b80b4d982ff8fbd4a52ea288a5b34de094f406
Zhuoyue Zhao <zyzhao@cs.utah.edu>
2017-11-07 22:45:58 -0700

    update

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
 class Map {
     constructor(states) {
-        console.log("map");
+        console.log(states);
 
-        this.width = 700;
-        this.height = 400;
+        this.width = 1000;
+        this.height = 550;
 
         let div = d3.select("#map");
         this.svg = div.append("svg")
@@ -18,15 +18,24 @@ class Map {
 
         this.projection = d3.geoAlbersUsa()
             .translate([this.width / 2, this.height / 2])
-            .scale([850]);
+            .scale([1150]);
 
         let path = d3.geoPath().projection(this.projection);
+        //this.mapLayer.selectAll("g")
+         //   .data(states.features)
+          //  .enter()
         this.mapLayer.selectAll("path")
             .data(states.features)
             .enter()
             .append("path")
             .attr("d", path)
             .classed("map-border", true);
+
+        this.markerLayer.append("circle")
+            .attr("cx", this.projection([-87.629798, 41.878114])[0])
+            .attr("cy", this.projection([-87.629798, 41.878114])[1])
+            .attr("r", 2)
+            .style("fill", "red");
     }
 
     update(banks) {
