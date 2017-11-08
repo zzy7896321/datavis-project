620d4432b78dee2d98164accb81b6dd04b6b7aa0
Zhuoyue Zhao <zyzhao@cs.utah.edu>
2017-11-08 01:58:24 -0700

    update

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 class Map {
     constructor(states) {
         console.log(states);
+        let thismap = this;
 
         this.width = 1000;
         this.height = 550;
@@ -21,9 +22,52 @@ class Map {
             .scale([1150]);
 
         let path = d3.geoPath().projection(this.projection);
-        //this.mapLayer.selectAll("g")
-         //   .data(states.features)
-          //  .enter()
+        let state_groups = this.mapLayer.selectAll("g")
+            .data(states.features)
+            .enter()
+            .append("g")
+            .attr("id", d => d.properties.state_abbv);
+
+        state_groups.append("path")
+            .attr("d", path)
+            .classed("map-border", true);
+        
+
+        state_groups.filter(d => d.properties.hasOwnProperty("lat2"))
+            .append("line")
+            .attr("x1", d => 
+                thismap.projection([d.properties.lng, d.properties.lat])[0] + (d.properties.state_abbv == "VT" ? 13 : 0))
+            .attr("y1", d =>
+                thismap.projection([d.properties.lng, d.properties.lat])[1] + (d.properties.state_abbv == "VT" ? 0 : -5))
+            .attr("x2", d => 
+                thismap.projection([d.properties.lng2, d.properties.lat2])[0])
+            .attr("y2", d =>
+                thismap.projection([d.properties.lng2, d.properties.lat2])[1])
+            .classed("map-line", true);
+
+        state_groups.filter(d => d.properties.state_abbv == "HI")
+            .append("line")
+            .attr("x1", d => 
+                thismap.projection([d.properties.lng, d.properties.lat])[0])
+            .attr("y1", d =>
+                thismap.projection([d.properties.lng, d.properties.lat])[1])
+            .attr("x2", d => 
+                thismap.projection([d.properties.lng, d.properties.lat])[0] + 30)
+            .attr("y2", d =>
+                thismap.projection([d.properties.lng, d.properties.lat])[1] - 5)
+            .classed("map-line", true);
+
+        let state_text = state_groups.append("text")
+            .attr("x", d => 
+                thismap.projection([d.properties.lng, d.properties.lat])[0] + (d.properties.state_abbv == "HI" ? 40: 0))
+            .attr("y", d =>
+                thismap.projection([d.properties.lng, d.properties.lat])[1])
+            .text(d => d.properties.state_abbv)
+            .classed("map-text", true);
+
+        state_text.filter(d => d.properties.hasOwnProperty("lat2"))
+            .style("text-anchor", "start");
+
         this.mapLayer.selectAll("path")
             .data(states.features)
             .enter()
@@ -34,7 +78,7 @@ class Map {
         this.markerLayer.append("circle")
             .attr("cx", this.projection([-87.629798, 41.878114])[0])
             .attr("cy", this.projection([-87.629798, 41.878114])[1])
-            .attr("r", 2)
+            .attr("r", 3)
             .style("fill", "red");
     }
 
