50639aaf7ac2c5ac5128032bf96dd3c9bd44ffba
Zhuoyue Zhao <zyzhao@cs.utah.edu>
2017-11-07 21:55:20 -0700

    update

--- js/map.js
+++ js/map.js
@@ -1,5 +1,4 @@
 class Map {
     constructor() {
-
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-let barChart = new BarChart();
-let map = new Map();
+function init() {
+    let barChart = new BarChart();
+    let map = new Map();
+    d3.csv("data/joined_table.csv", function (error, banks) {
+        let lineChart = new LineChart(barChart, map, banks);
+        lineChart.update();
+    });
+}
 
-
-d3.csv("data/joined_table.csv", function (error, banks) {
-    let lineChart = new LineChart(barChart, map, banks);
-    lineChart.update();
-});
 
